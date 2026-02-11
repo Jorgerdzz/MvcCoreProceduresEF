007272f44d8869b194974c61052ee27fc285f5db
Jorgerdzz <asilgalan083@gmail.com>
2026-02-11 10:34:42 +0100

    CRUD Enfermos finalizado

--- MvcCoreProceduresEF/Controllers/EnfermosController.cs
+++ MvcCoreProceduresEF/Controllers/EnfermosController.cs
@@ -17,5 +17,30 @@ namespace MvcCoreProceduresEF.Controllers
             List<Enfermo> enfermos = await this.repo.GetEnfermosAsync();
             return View(enfermos);
         }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            Enfermo enfermo = await this.repo.FindEnfermoAsync(id);
+            return View(enfermo);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            await this.repo.DeleteEnfermoRawAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Enfermo enf)
+        {
+            await this.repo.CreateEnfermoAsync(enf.Inscripcion, enf.Apellido, enf.Direccion, enf.FechaNacimiento, enf.Genero, enf.Nss);
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- MvcCoreProceduresEF/Repositories/RepositoryEnfermos.cs
+++ MvcCoreProceduresEF/Repositories/RepositoryEnfermos.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using MvcCoreProceduresEF.Data;
 using MvcCoreProceduresEF.Models;
 using System;
+using System.Collections.Generic;
 using System.Data.Common;
 
 namespace MvcCoreProceduresEF.Repositories
@@ -23,6 +26,12 @@ namespace MvcCoreProceduresEF.Repositories
     //(@inscripcion nvarchar(50))
     //as
     //	delete from ENFERMO where INSCRIPCION = @inscripcion
+    //go
+
+    //create procedure SP_INSERT_ENFERMO
+    //(@inscripcion nvarchar(50), @apellido nvarchar(50), @direccion nvarchar(50), @fechaNacimiento DateTime, @genero nvarchar(50), @nss nvarchar(50))
+    //as
+    //    insert into ENFERMO values(@inscripcion, @apellido, @direccion, @fechaNacimiento, @genero, @nss)
     //go
     #endregion
     public class RepositoryEnfermos
@@ -65,6 +74,60 @@ namespace MvcCoreProceduresEF.Repositories
                 await com.Connection.CloseAsync();
                 return enfermos;
             }
+        }
+
+        public async Task<Enfermo> FindEnfermoAsync(string inscripcion)
+        {
+            //PARA LLAMAR A UN PROCEDIMIENTO QUE CONTIENE PARAMETROS
+            //LA LLAMADA SE REALIZA MEDIANTE EL NOMBRE DEL PROCEDURE
+            //Y CADA PARAMETRO A CONTINUAION EN LA DECLARACION DEL SQL:
+            //SP_PROCEDURE @PAM1, @PAM2
+            string sql = "SP_BUSCAR_ENFERMO @inscripcion";
+            SqlParameter pamIns = new SqlParameter("@inscripcion", inscripcion);
+            //SI LOS DATOS QUE DEVUELVE EL PROCEDURE ESTAN MAPEADOS CON UN MODEL
+            //PODEMOS UTILIZAR EL METODO FromSqlRaw PARA RECUPERAR DIRECTAMENTE EL MODEL/S
+            //NO PODEMOS CONSULTAR Y EXTRAER A LA VEZ CON LINQ, SE DEBE REALIZAR
+            //SIEMPRE EN DOS PASOS
+            //var consulta = this.context.Enfermos.FromSqlRaw(sql, pamIns).ToList();
+            var consulta = this.context.Enfermos.FromSqlRaw(sql, pamIns);
+            Enfermo enfermo = await consulta.AsAsyncEnumerable().FirstOrDefaultAsync();
+            //Enfermo enfermo = consulta.FirstOrDefault();
+            return enfermo;
+        }
+
+        public async Task DeleteEnfermoAsync(string inscripcion)
+        {
+            string sql = "SP_DELETE_ENFERMO";
+            SqlParameter pamIns = new SqlParameter("@inscripcion", inscripcion);
+            using (DbCommand com = this.context.Database.GetDbConnection().CreateCommand())
+            {
+                com.CommandType = System.Data.CommandType.StoredProcedure;
+                com.CommandText = sql;
+                com.Parameters.Add(pamIns);
+                await com.Connection.OpenAsync();
+                await com.ExecuteNonQueryAsync();
+                await com.Connection.CloseAsync();
+                com.Parameters.Clear();
+            }
+        }
+        
+        public async Task DeleteEnfermoRawAsync(string inscripcion)
+        {
+            string sql = "SP_DELETE_ENFERMO @inscripcion";
+            SqlParameter pamIns = new SqlParameter("@inscripcion", inscripcion);
+            await this.context.Database.ExecuteSqlRawAsync(sql, pamIns);
+        }
+
+        public async Task CreateEnfermoAsync(string inscripcion, string apellido, string direccion, DateTime fechaNac, string genero, string nss)
+        {
+            string sql = "SP_INSERT_ENFERMO @inscripcion, @apellido, @direccion, @fechaNacimiento, @genero, @nss";
+            SqlParameter pamIns = new SqlParameter("@inscripcion", inscripcion);
+            SqlParameter pamApe = new SqlParameter("@apellido", apellido);
+            SqlParameter pamDir = new SqlParameter("@direccion", direccion);
+            SqlParameter pamFech = new SqlParameter("@fechaNacimiento", fechaNac);
+            SqlParameter pamGen = new SqlParameter("@genero", genero);
+            SqlParameter pamNs = new SqlParameter("@nss", nss);
+            await this.context.Database.ExecuteSqlRawAsync(sql, pamIns, pamApe, pamDir, pamFech, pamGen, pamNs);
         }
 
     }
